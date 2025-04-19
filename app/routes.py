78fb07ed65ef174cccdf33f48862f6a322fcde64
AymenXcep <aymanchabbaki09@gmail.com>
2025-04-19 22:15:08 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def create_preprocessor(categorical_features, numerical_features):
 LEAGUE_MODELS = {
     'epl': {
         'winner': {
-            'model': joblib.load('./models/epl_winner.pkl'),
+            'model': joblib.load('./app/models/epl_winner.pkl'),
             'categorical_features': ['strHomeTeam', 'strAwayTeam'],
             'numerical_features': [
                 'homeGD', 'homeWinRate', 'homeClassement', 'predictedIntHomeScore', 'predictedResult',
@@ -41,7 +41,7 @@ LEAGUE_MODELS = {
             ]
         },
         'goals': {
-            'model': joblib.load('./models/epl_goals.pkl'),
+            'model': joblib.load('./app/models/epl_goals.pkl'),
             'categorical_features': ['strHomeTeam', 'strAwayTeam'],
             'numerical_features': [
                 'homeGD', 'homeWinRate', 'homeClassement', 'lastHome5GamesScore',
